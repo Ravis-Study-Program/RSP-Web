287bf82164ddfc8579a794bc7da3b0fd302ff3fd
Jason Beh <behjieshen@gmail.com>
2025-04-13 12:05:40 +1000

    Enhance graduates page

--- client/src/pages/Graduates/Graduates.page.tsx
+++ client/src/pages/Graduates/Graduates.page.tsx
@@ -1,18 +1,8 @@
-import { IconBrandDiscordFilled } from '@tabler/icons-react';
-import {
-  ActionIcon,
-  Anchor,
-  Avatar,
-  Button,
-  Card,
-  Grid,
-  Group,
-  rem,
-  Skeleton,
-  Text,
-} from '@mantine/core';
+import { useMemo, useState } from 'react';
+import { Avatar, Button, Card, Flex, Grid, MultiSelect, Skeleton, Text } from '@mantine/core';
 import { Layout } from '@/components/Layout/Layout';
 import { GraduateDto, useGetGraduates } from '@/generated/api/client';
+import { createOptionsFilter, getGraduates } from '@/shared/table/globalFilters';
 import classes from './Graduates.module.css';
 
 export default function GraduatesPage() {
@@ -22,12 +12,44 @@ export default function GraduatesPage() {
     isFetching: isFetchingGraduates,
     isLoading: isLoadingGraduates,
   } = useGetGraduates();
+  const [selectedGraduates, setSelectedGraduates] = useState<string[]>([]);
+  const graduatesOptions = getGraduates(graduatesResponse?.responseBody?.graduates || []);
+
+  const filteredGraduates = useMemo(() => {
+    const graduates = graduatesResponse?.responseBody?.graduates || [];
+
+    if (selectedGraduates.length === 0) {
+      return graduates;
+    }
+
+    return graduates.filter(
+      (graduate) => graduate.name != null && selectedGraduates.includes(graduate.name)
+    );
+  }, [graduatesResponse, selectedGraduates]);
 
   return (
     <Layout>
-      <Grid gutter={{ base: 'md', xs: 'md', md: 'xl', xl: 50 }}>
+      <Flex mb="xl" pb="xl" justify="space-between" align="flex-end">
+        <MultiSelect
+          classNames={{ inputField: classes.inputField }}
+          label="Name"
+          placeholder="Pick value(s)"
+          data={graduatesOptions}
+          filter={createOptionsFilter()}
+          miw={150}
+          searchable
+          nothingFoundMessage="Nothing found..."
+          value={selectedGraduates}
+          onChange={(values) => {
+            setSelectedGraduates(values as string[]);
+          }}
+        />
+        <Text size="sm">{filteredGraduates.length} found</Text>
+      </Flex>
+
+      <Grid mt="lg" gutter={{ base: 'md', xs: 'md', md: 'xl', xl: 50 }}>
         {!isLoadingGraduates && !isFetchingGraduates && !isLoadingGraduatesError ? (
-          <GraduateCards graduates={graduatesResponse?.responseBody?.graduates} />
+          <GraduateCards graduates={filteredGraduates} />
         ) : (
           <GraduateSkeletonCards />
         )}
@@ -66,9 +88,15 @@ export function GraduateCards({ graduates }: GraduateCardsProps) {
     <>
       {graduates?.map((graduate, key) => (
         <Grid.Col key={key} span={{ base: 12, sm: 6, md: 6, lg: 2 }}>
-          <Card key={key} withBorder shadow="sm" radius="md" className={classes.card}>
-            <Avatar src={graduate.profileImage} size={70} radius={70} mx="auto" />
-            <Text ta="center" fz="lg" fw={600} mt="md">
+          <Card key={key} withBorder shadow="sm" radius="md" py="xl" className={classes.card}>
+            <Avatar
+              color="initials"
+              name={graduate?.name || undefined}
+              size={60}
+              radius={60}
+              mx="auto"
+            />
+            <Text ta="center" fz="lg" fw={600} size="sm" mt="md">
               {graduate.name}
             </Text>
 
@@ -77,23 +105,11 @@ export function GraduateCards({ graduates }: GraduateCardsProps) {
               href={`/profile?email=${graduate.email}`}
               radius="md"
               mt="sm"
-              size="sm"
+              size="xs"
               variant="primary"
             >
-              View Profile
+              Profile
             </Button>
-
-            <Group gap={0} mt="md">
-              <Anchor c="gray" target="_blank" href="https://www.discord.com">
-                <ActionIcon variant="subtle" color="gray">
-                  <IconBrandDiscordFilled
-                    style={{ width: rem(20), height: rem(20) }}
-                    color="gray"
-                    stroke={1.5}
-                  />
-                </ActionIcon>
-              </Anchor>
-            </Group>
           </Card>
         </Grid.Col>
       ))}
