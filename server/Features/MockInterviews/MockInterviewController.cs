083c38b61a4a2258f9ee75e664b6b976227845d8
YuBaichuan2000 <2540899235yu@gmail.com>
2025-12-13 19:26:24 +1100

    added back get all mock interview controller method

--- server/Features/MockInterviews/MockInterviewController.cs
+++ server/Features/MockInterviews/MockInterviewController.cs
@@ -50,7 +50,20 @@ public class MockInterviewController : BaseController
   [ServiceFilter(typeof(AuthAttribute))]
   [Route("get")]
   [ActionName("ListMockInterview")]
-  public async Task<ActionResult<ApiResponse<ListMockInterviewCursorResponse>>> ListMockInterview(
+  public async Task<ActionResult<ApiResponse<ListMockInterviewResponse>>> ListMockInterview(
+    [FromQuery] ListMockInterviewRequest request,
+    CancellationToken cancellationToken = default
+  )
+  {
+    var result = await _mockInterviewService.ListMockInterview(request, cancellationToken);
+    return OkResponse(result, Messages.MockInterview.Listed);
+  }
+
+  [HttpGet]
+  [ServiceFilter(typeof(AuthAttribute))]
+  [Route("get-paginated")]
+  [ActionName("ListPaginatedMockInterview")]
+  public async Task<ActionResult<ApiResponse<ListMockInterviewCursorResponse>>> ListPaginatedMockInterview(
     [FromQuery] ListMockInterviewRequest request,
     CancellationToken cancellationToken = default
   )
